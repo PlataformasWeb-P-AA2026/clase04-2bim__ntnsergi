70d56dd5d68630fb05223df0894f8975237c5f18
ntnsergi <sergi.galarzaend@gmail.com>
2026-06-25 11:27:25 -0500

    v2

--- ejemplo2/proyectoUno/administrativo/models.py
+++ ejemplo2/proyectoUno/administrativo/models.py
@@ -1,3 +1,4 @@
+# pyrefly: ignore [missing-import]
 from django.db import models
 
 # Create your models here.
@@ -12,6 +13,9 @@ class Estudiante(models.Model):
                 self.apellido,
                 self.cedula)
 
+    def obtener_numeros_telefonicos(self):
+        return self.numeros_telefonicos.count()
+
 class NumeroTelefonico(models.Model):
     telefono = models.CharField(max_length=100)
     tipo = models.CharField(max_length=100)
@@ -20,4 +24,12 @@ class NumeroTelefonico(models.Model):
 
     def __str__(self):
         return "%s %s" % (self.telefono, self.tipo)
+
+    def obtener_operadora(self):
+        if self.telefono.startswith("098"):
+            return "Movistar "
+        elif self.telefono.startswith("099"):
+            return "Claro "
+        else:
+            return "Convencional "
 
